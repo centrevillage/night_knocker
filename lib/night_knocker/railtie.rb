1a78e68a85aa53142288bd2bd88ac094a9aa1f40
centrevillage <centrevillage@gmail.com>
2014-04-18 12:02:09 +0900

    remove asset precompile.

--- lib/night_knocker/railtie.rb
+++ lib/night_knocker/railtie.rb
@@ -2,10 +2,6 @@ require 'rails'
 
 module NightKnocker
   class Railtie < ::Rails::Railtie
-    config.before_initialize do |app|
-      config.assets.precompile += %w(night_knocker.js)
-    end
-
     config.to_prepare do
       NightKnocker::Env.setup
     end
